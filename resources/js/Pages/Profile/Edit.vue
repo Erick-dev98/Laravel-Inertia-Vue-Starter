f8a9867c6c16d74905926ed800d7f9f02b7cc7b0
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-10-14 06:02:01 +0300

    first commit

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,5 +1,19 @@
-<script setup></script>
+<script setup>
+import DeleteAccount from './Sections/DeleteAccount.vue';
+import UpdateInfo from './Sections/UpdateInfo.vue';
+import UpdatePassword from './Sections/UpdatePassword.vue';
+
+defineProps({
+    user: Object,
+    status: String
+})
+</script>
 
 <template>
-    <div>Profile</div>
+
+    <Head title="- Profile" />
+    <!-- accept the user prop here in the child component  -->
+    <UpdateInfo :user="user" :status="status" />
+    <UpdatePassword />
+    <DeleteAccount />
 </template>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,26 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->middleware('verified')
+        ->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'updateInfo'])
+        ->name('profile.info');
+    Route::put('/profile', [ProfileController::class, 'updatePassword'])
+        ->name('profile.password');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
+});
 
 Route::inertia('/', 'Home')->name('home');
 
-Route::inertia('/dashboard', 'Dashboard')->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::inertia('/profile', 'Profile/Edit')->middleware(['auth', 'password.confirm'])
-    ->name('profile.edit');
-
+// Route::inertia('/profile', 'Profile/Edit')
+//     ->middleware(['auth', 'password.confirm'])
+//     ->name('profile.edit');
 require __DIR__ . '/auth.php';
